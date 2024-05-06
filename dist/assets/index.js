f7a3857c4bb2457bb086e30804fc1dfa743779a9
Martijn Kooijman <martijnkooijman12@gmail.com>
2024-05-06 20:56:05 +0200

    Image fix

--- dist/assets/index.js
+++ dist/assets/index.js
@@ -1,6 +1,1 @@
-const cards = document.querySelectorAll(".div");
-cards.forEach((card) => {
-  card.addEventListener("click", function() {
-    console.log("Je hebt op een card geklikt");
-  });
-});
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,3 @@
 import '../css/index.css';
 import '../img/logo.png';
 import '../img/coding.svg';
-
-const cards = document.querySelectorAll('.div');
-cards.forEach((card) => {
-  card.addEventListener('click', function() {
-    // Log dat er op de card is geklikt
-    console.log('Je hebt op een card geklikt');
-  });
-});
