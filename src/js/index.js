9e6b71760ba610d4bbc9df402d0d75dbc89fd13e
Martijn Kooijman <martijnkooijman12@gmail.com>
2024-05-06 02:01:13 +0200

    First styling

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,9 @@
 import '../css/index.css';
 
-import gsap from "gsap";
-
-window.addEventListener('scroll', function () {
-  var scrollPosition = window.scrollY;
-  var header = document.querySelector('header');
-  var body = document.querySelector('body');
-
-  if (scrollPosition > 0) {
-    header.style.backgroundColor = '#0E1122';
-  } else if(header.classList.contains("menu-open")) {
-    body.style.overflow = "hidden";
-        header.style.backgroundColor = '#0E1122';
-  }
+const cards = document.querySelectorAll('.div');
+cards.forEach((card) => {
+  card.addEventListener('click', function() {
+    // Log dat er op de card is geklikt
+    console.log('Je hebt op een card geklikt');
+  });
 });
-
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const header = document.querySelector('header');
-
-hamburgerMenu.addEventListener('click', () => {
-  header.classList.toggle('menu-open');
-});
-
-// gsap.to('.search-card', {
-//   opacity: 1,
-//   duration: 1,
-//   stagger: 0.1 // dit zorgt ervoor dat elke kaart 0.5 seconden na de vorige infade
-// });
