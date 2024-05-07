df9dbe1c9bb2cbdec955fc39ba74a7133acfe62a
Martijn Kooijman <martijnkooijman12@gmail.com>
2024-05-07 11:41:36 +0200

    Added styling and id to data

--- dist/assets/index.js
+++ dist/assets/index.js
@@ -1,1 +1,6 @@
-
+var cards = document.querySelectorAll(".card");
+cards.forEach(function(card) {
+  card.addEventListener("click", function() {
+    console.log("Je hebt op card " + this.id + " geklikt");
+  });
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 import '../css/index.css';
 import '../img/logo.png';
 import '../img/coding.svg';
+
+var cards = document.querySelectorAll('.card');
+
+cards.forEach(function(card) {
+  card.addEventListener('click', function() {
+    console.log('Je hebt op card ' + this.id + ' geklikt');
+  });
+});
